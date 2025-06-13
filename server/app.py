78747924abeef93eafb5902e4f29a2a3eb521844
Fancy Chepngetich Byegon <fancychepngetichbyegon@gmail.com>
2025-06-13 07:19:28 +0300

    flask sqlalchemy server

--- server/app.py
+++ server/app.py
@@ -1,27 +1,20 @@
-# server/app.py
-#!/usr/bin/env python3
-
-from flask import Flask, make_response
+from flask import Flask, jsonify
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///earthquakes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
+migrate = Migrate(app, db)
 
-
-@app.route('/')
-def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
-    return make_response(body, 200)
-
-# Add views here
-
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return jsonify(earthquake.to_dict()), 200
+    return jsonify({"message": f"Earthquake {id} not found."}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -1,20 +1,14 @@
-#!/usr/bin/env python3
-# server/seed.py
-
-from app import app
-from models import db, Earthquake
+from app import app, db
+from models import Earthquake
 
 with app.app_context():
+    db.drop_all()
+    db.create_all()
 
-    # Delete all rows in the "earthquakes" table
-    Earthquake.query.delete()
+    e1 = Earthquake(magnitude=9.5, location="Chile", year=1960)
+    e2 = Earthquake(magnitude=9.2, location="Alaska", year=1964)
 
-    # Add several Earthquake instances to the "earthquakes" table
-    db.session.add(Earthquake(magnitude=9.5, location="Chile", year=1960))
-    db.session.add(Earthquake(magnitude=9.2, location="Alaska", year=1964))
-    db.session.add(Earthquake(magnitude=8.6, location="Alaska", year=1946))
-    db.session.add(Earthquake(magnitude=8.5, location="Banda Sea", year=1934))
-    db.session.add(Earthquake(magnitude=8.4, location="Chile", year=1922))
-
-    # Commit the transaction
+    db.session.add_all([e1, e2])
     db.session.commit()
+
+    print("Database seeded!")
